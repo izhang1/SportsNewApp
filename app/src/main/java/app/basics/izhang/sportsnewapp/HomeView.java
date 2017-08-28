764ba9adad3426e7012103bd6d8665ebda2c2720
Ivan Zhang <ivanzhangemail@gmail.com>
2017-08-28 19:25:57 -0400

    Initial Commit

---
+++ app/src/main/java/app/basics/izhang/sportsnewapp/HomeView.java
@@ -0,0 +1,13 @@
+package app.basics.izhang.sportsnewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_view);
+    }
+}
